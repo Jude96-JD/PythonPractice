d3490c602ed18ceaa361f5f7b838ad2dcc30d823
Jude Dsouza <Jude_Dsouza@outlook.com>
2026-02-02 17:23:23 +0530

    variables & datatypes

---
+++ 2/03_rules_variables.py
@@ -0,0 +1,12 @@
+a = 23
+
+aaa = 435
+
+jude = 34
+
+jordan = 45
+
+_jordan = 34
+
+# @jordan = 56 # Invalid due to @ symbol
+# jord@n = 56 # Invalid due to @ symbol
---
+++ 2/05_type.py
@@ -0,0 +1,5 @@
+a = "31.2"
+b = float(a) # a but the type should be float # converting float to integer will be invalid in this case
+t = type(b)
+
+print(t)
