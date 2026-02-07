9f6919d33c3ef1b0ddbfb60a5422267f71f71246
Jude Dsouza <Jude_Dsouza@outlook.com>
2026-02-07 15:36:47 +0530

    Strings

---
+++ 3 - PS/04_problem4.py
@@ -0,0 +1,4 @@
+name = "Jude is a good  boy"
+
+print(name.replace("  ", " "))
+print(name) # Without the replace method, print(name) will print the original string
---
+++ 3 - PS/05_problem5.py
@@ -0,0 +1,3 @@
+letter  = "Dear Jude,\n\tYou've been selected in this program.\nThanks!"
+
+print(letter)
