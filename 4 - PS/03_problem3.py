cd3a28487cc00837ca6146ce9f22c950e4b2f02f
Jude Dsouza <Jude_Dsouza@outlook.com>
2026-02-11 10:03:15 +0530

    List & Tuples

---
+++ 4 - PS/03_problem3.py
@@ -0,0 +1,5 @@
+# Check that tuple type cannot be changed in python
+
+a = (34, 234, "Jordan")
+
+a[2] = "Gordan"
---
+++ 4 - PS/04_problem4.py
@@ -0,0 +1,5 @@
+# Write a program to sum a list with 4 numbers
+
+l = [7, 2, 5, 9]
+
+print(sum(l))
