fedc9b3565a92ce0d23647edacde221ffcab2783
Jude Dsouza <Jude_Dsouza@outlook.com>
2026-02-13 08:21:01 +0530

    dictionary & sets

---
+++ 5 - PS/05_problem5.py
@@ -0,0 +1,5 @@
+# s = {}
+# What is the type of 's'?
+
+s = {} # It is a dictionary type
+print(type(s))
---
+++ 5 - PS/09_problem9.py
@@ -0,0 +1,5 @@
+# Can you change the values inside a list which is contained in set S?
+
+s = {8, 7, 12, "Jordan", [1, 2]}
+
+# No, you cannot change the value inside a list contained in a set in Python. Sets in Python require all their elements to be immutable and hashable
